bfca22dc08c447e743a41b10850624d94038d9e6
topikiviluoma <topi.kiviluoma@helsinki.fi>
2018-08-22 09:44:33 +0300

    viikon 4 alkua

---
+++ viikko4/blogilista/src/index.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+const mongoUrl = process.env.BLOGIDB_URI
+mongoose.connect(mongoUrl)
+
+
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
