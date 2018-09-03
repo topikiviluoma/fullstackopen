987a465e7da4838501e3f75e6e244aff29952c71
Topi Kiviluoma <topi.kiviluoma@helsinki.fi>
2018-09-03 08:50:35 +0300

    viikkoa 4

--- viikko4/blogilista/src/controllers/blogs.js
+++ viikko4/blogilista/src/controllers/blogs.js
@@ -1,22 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/Blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+blogsRouter.get('/', async (request, response) => {
+  try {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(Blog.format))
+  } catch (e) {
+    console.log(e)
+    response.status(404)
+  }
 
-  module.exports = blogsRouter
+
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  console.log(blog)
+  if (blog.likes === undefined) {
+    blog.likes = 0
+  }
+  if (blog.title === undefined || blog.author === undefined) {
+    return response.status(401).json({error: 'wut'})
+  }
+  console.log('helloouuu')
+  const result = await blog.save()
+  response.status(201).json(Blog.format(result))
+
+
+})
+
+module.exports = blogsRouter
--- viikko4/blogilista/src/models/Blog.js
+++ viikko4/blogilista/src/models/Blog.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose')
 
-const Blog = mongoose.model('Blog', {
+const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
 })
+
+blogSchema.statics.format = (blog) => {
+  return {
+      id: blog._id,
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes
+  }
+}
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
 
 module.exports = Blog
